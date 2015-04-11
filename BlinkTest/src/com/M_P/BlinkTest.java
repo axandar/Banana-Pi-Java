94bb2d16a941fc1a249bfe8aa63878a43acb8c07
Axandar <axandar29@gmail.com>
2015-04-12 01:30:42 +0200

    Adding programs
    Adding few programs

---
+++ BlinkTest/src/com/M_P/BlinkTest.java
@@ -0,0 +1,62 @@
+package com.M_P;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BlinkTest{
+	
+	public static void main(String[] args) throws Exception{
+		
+		/**if (Gpio.wiringPiSetup() == -1) {
+            System.out.println(" ==>> GPIO SETUP FAILED");
+            return;
+        }
+		
+		Gpio.pinMode(7, Gpio.OUTPUT);
+		
+		for(int n = 0; n < 10; n++){
+			Gpio.digitalWrite(1, 1);
+			System.out.println("Pin 1 wartosc 1");
+			Gpio.delay(1000);
+			Gpio.digitalWrite(1, 0);
+			System.out.println("Pin 1 wartosc 0");
+			Gpio.delay(1000);
+		}**/
+		
+		/**LcdDisplay lcd = new LcdDisplay();
+
+		lcd.I2CLcdDisplay(2, 16, 2, 0x38, 7, 6, 5, 4, 3, 2, 1, 0);**/
+		
+		/**Sftp sftp = new Sftp();
+		
+		sftp.connect("77.91.51.60", "axandar", "lkj321", 22);
+		sftp.channelsftp(null);
+		sftp.send("D:/loremipsum.txt");
+		sftp.download("loremipsum.txt", "D:/loremipsum2.txt");
+		sftp.close_connection();**/
+		
+		String ilosc_miejsca = ZajetoscDysku();
+		System.out.println(ilosc_miejsca);
+
+		return;
+	}
+	
+	public static String ZajetoscDysku() throws IOException{
+		String s = null;
+		String wynik = null;
+		int gdzie_m = 0;
+		try{
+			String[] command = { "du", "-hs", "/home" };         
+			Process process = Runtime.getRuntime().exec(command);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(        
+			process.getInputStream()));
+			s = reader.readLine();
+			gdzie_m = s.indexOf("M");
+			wynik = s.substring(0, gdzie_m);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return wynik;
+	}
+}
---
+++ Check_temp/src/com/Axandar/Main.java
@@ -0,0 +1,52 @@
+package com.Axandar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static int temp;
+	
+	
+	public static void main(String[] args) throws Exception{
+		while(true){
+			try{
+				temp = Temperature();
+				System.out.println(temp);
+				if(temp>28000){
+					System.out.println("WYLACZANIE");
+					TurnOff();
+				}
+				Thread.sleep(5000);
+			}catch(InterruptedException e){
+				System.out.println("zosta³em obudzony przedwczeœnie");
+			}
+		}
+
+	}
+	public static int Temperature() throws IOException{
+		String s = null;
+		int output = 0;
+		try{
+			String[] command = { "cat", "/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input"}; 
+			Process process = Runtime.getRuntime().exec(command);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(        
+			process.getInputStream()));
+			s = reader.readLine();
+			output = Integer.parseInt(s);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return output;
+	}
+	public static void TurnOff(){
+		try{
+			String[] command = { "shutdown", "-h", "now"}; 
+			@SuppressWarnings("unused")
+			Process process = Runtime.getRuntime().exec(command);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
